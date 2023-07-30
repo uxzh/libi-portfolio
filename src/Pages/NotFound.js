6c34b9d3964c301f600c57d385b246b16b3d50bb
uxzh <77202660+uxzh@users.noreply.github.com>
2023-07-30 13:20:27 +0300

    Update NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,10 +1,10 @@
 import React from "react";
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <h1 style={{ fontSize: "40vw", margin: 0 }}>404</h1>
-      <h2>Page not found!</h2>
+    <div className="not-found" style={{ height: "100%", margin: "auto" }}>
+      <h1 style={{ fontSize: 82, margin: "auto", marginTop: "8vh" }}>404</h1>
       <center>
+        <h3>Page not found!</h3>
         Sorry, the page you are looking for<br></br> does not exist.
       </center>
       <center style={{ paddingTop: "4vh" }}>
